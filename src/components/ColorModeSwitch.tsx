9729524d6266c0faadc53612b1ed43eabd99da01
Tedros Alem <tedsaasfaha@gmail.com>
2023-06-25 14:26:51 +0300

    Fetch the games

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -5,7 +5,7 @@ const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
 
   return (
-    <HStack justifyContent="space-between">
+    <HStack>
       <Switch
         colorScheme="green"
         isChecked={colorMode === "dark"}
